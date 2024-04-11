d5a116b02af8c316929b5b65d5f25dbfe9a40568
marilianewton <mariliagomesnewton@gmail.com>
2024-04-11 12:07:03 -0300

    Add javascript functionalities

--- script.js
+++ script.js
@@ -1,33 +1,93 @@
 const containerHeader = document.querySelector(".container-header");
 const containerGrid = document.querySelector(".container-grid");
-
 const btnGridOnOff = document.querySelector(".btn-grid");
+const rangeGrid = document.querySelector(".range-grid");
+const colorPicker = document.querySelector(".input-color");
+
+let squaresGrid;
 let grid = true;
+let initialGrid = 10;
 
 function createGrid(size) {
+  containerGrid.innerHTML = "";
   for (let x = 0; x < size; x++) {
     for (let y = 0; y < size; y++) {
       const square = document.createElement("div");
       square.classList.add("square");
-      console.log(containerGrid.offsetHeight);
+      square.classList.add("grid-on");
       square.style.height = `${containerGrid.offsetHeight / size}px`;
       square.style.width = `${containerGrid.offsetWidth / size}px`;
+      // Value used for progressive darkening later
+      square.value = 0.1;
 
-      console.log(containerGrid.offsetWidth);
-      console.log(size);
       containerGrid.appendChild(square);
+
+      // Get a node list with all squares on the grid
+      squaresGrid = document.querySelectorAll(".square");
     }
   }
+  colourSquare();
+}
+
+function colourSquare() {
+  let canColour = false;
+
+  document.addEventListener("mousedown", () => (canColour = true));
+  document.addEventListener("mouseup", () => (canColour = false));
+
+  squaresGrid.forEach((sq) => {
+    ["mousedown", "mouseover"].forEach(function (e) {
+      sq.addEventListener(e, function () {
+        if (!canColour && e !== "mousedown") return;
+
+        const colorChoice = document.querySelector(
+          ".color-choice:checked"
+        ).value;
+
+        if (colorChoice === "color") {
+          sq.style.backgroundColor = `${colorPicker.value}`;
+          sq.value = 0.1;
+        }
+        if (colorChoice === "eraser") {
+          sq.style.backgroundColor = `var(--baby-powder)`;
+          sq.value = 0.1;
+        }
+        if (colorChoice === "random") {
+          sq.style.backgroundColor = `rgb(${Math.floor(
+            Math.random() * 256
+          )},${Math.floor(Math.random() * 256)},${Math.floor(
+            Math.random() * 256
+          )})`;
+          sq.value = 0.1;
+        }
+        if (colorChoice === "progressive") {
+          let increment = 0.1;
+          sq.style.backgroundColor = `rgba(0,0,0,${(sq.value += increment)})`;
+        }
+      });
+    });
+  });
 }
 
 btnGridOnOff.addEventListener("click", function () {
   if (grid) {
     btnGridOnOff.style.transform = "rotate(70deg)";
     grid = false;
+    squaresGrid.forEach((sq) => sq.classList.remove("grid-on"));
   } else {
     btnGridOnOff.style.transform = "rotate(0deg)";
     grid = true;
+    squaresGrid.forEach((sq) => sq.classList.add("grid-on"));
   }
 });
 
-createGrid(12);
+rangeGrid.addEventListener("change", function () {
+  createGrid(rangeGrid.value);
+});
+
+function init() {
+  createGrid(initialGrid);
+  rangeGrid.value = initialGrid;
+}
+
+init();
