a866a0b393b810294ae4cfe41a985bb9afb043d1
marilianewton <mariliagomesnewton@gmail.com>
2024-04-11 21:17:53 -0300

    Put js script after html on index

--- script.js
+++ script.js
@@ -1,106 +1,105 @@
-setTimeout(() => {
-  const containerGrid = document.querySelector(".container-grid");
-  const btnGridOnOff = document.querySelector(".btn-grid");
-  const rangeGrid = document.querySelector(".range-grid");
-  const colorPicker = document.querySelector(".input-color");
-  const btnClearAll = document.querySelector(".btn-clear");
-  const radioColorPicker = document.getElementById("color");
+const containerHeader = document.querySelector(".container-header");
+const containerGrid = document.querySelector(".container-grid");
+const btnGridOnOff = document.querySelector(".btn-grid");
+const rangeGrid = document.querySelector(".range-grid");
+const colorPicker = document.querySelector(".input-color");
+const btnClearAll = document.querySelector(".btn-clear");
+const radioColorPicker = document.getElementById("color");
 
-  let squaresGrid;
-  let grid = true;
-  let initialGrid = 10;
-  let canColour = false;
+let squaresGrid;
+let grid = true;
+let initialGrid = 10;
+let canColour = false;
 
-  document.addEventListener("mousedown", () => (canColour = true));
-  document.addEventListener("mouseup", () => (canColour = false));
+document.addEventListener("mousedown", () => (canColour = true));
+document.addEventListener("mouseup", () => (canColour = false));
 
-  btnGridOnOff.addEventListener("click", function () {
-    if (grid) {
-      btnGridOnOff.style.transform = "rotate(70deg)";
-      grid = false;
-      squaresGrid.forEach((sq) => sq.classList.remove("grid-on"));
-    } else {
-      btnGridOnOff.style.transform = "rotate(0deg)";
-      grid = true;
-      squaresGrid.forEach((sq) => sq.classList.add("grid-on"));
-    }
-  });
-
-  btnClearAll.addEventListener("click", function () {
-    squaresGrid.forEach((sq) => {
-      sq.style.backgroundColor = `var(--baby-powder)`;
-      sq.value = 0.1;
-    });
-  });
-
-  rangeGrid.addEventListener("change", function () {
-    createGrid(rangeGrid.value);
-  });
-
-  colorPicker.addEventListener("click", function () {
-    radioColorPicker.checked = true;
-  });
-
-  function createGrid(size) {
-    containerGrid.innerHTML = "";
-    for (let x = 0; x < size; x++) {
-      for (let y = 0; y < size; y++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.classList.add("grid-on");
-        square.style.height = `${containerGrid.offsetHeight / size}px`;
-        square.style.width = `${containerGrid.offsetWidth / size}px`;
-        // Value used for progressive darkening later
-        square.value = 0.1;
-
-        containerGrid.appendChild(square);
-
-        // Get a node list with all squares on the grid
-        squaresGrid = document.querySelectorAll(".square");
-      }
-    }
-    colourSquare();
+btnGridOnOff.addEventListener("click", function () {
+  if (grid) {
+    btnGridOnOff.style.transform = "rotate(70deg)";
+    grid = false;
+    squaresGrid.forEach((sq) => sq.classList.remove("grid-on"));
+  } else {
+    btnGridOnOff.style.transform = "rotate(0deg)";
+    grid = true;
+    squaresGrid.forEach((sq) => sq.classList.add("grid-on"));
   }
+});
 
-  function colourSquare() {
-    squaresGrid.forEach((sq) => {
-      ["mousedown", "mouseover"].forEach(function (eventName) {
-        sq.addEventListener(eventName, function () {
-          if (!canColour && eventName !== "mousedown") return;
+btnClearAll.addEventListener("click", function () {
+  squaresGrid.forEach((sq) => {
+    sq.style.backgroundColor = `var(--baby-powder)`;
+    sq.value = 0.1;
+  });
+});
 
-          const colorChoice = document.querySelector(
-            ".color-choice:checked"
-          ).value;
+rangeGrid.addEventListener("change", function () {
+  createGrid(rangeGrid.value);
+});
 
-          if (colorChoice === "color") {
-            sq.style.backgroundColor = `${colorPicker.value}`;
-            sq.value = 0.1;
-          }
-          if (colorChoice === "eraser") {
-            sq.style.backgroundColor = `var(--baby-powder)`;
-            sq.value = 0.1;
-          }
-          if (colorChoice === "random") {
-            sq.style.backgroundColor = `rgb(${Math.floor(
-              Math.random() * 256
-            )},${Math.floor(Math.random() * 256)},${Math.floor(
-              Math.random() * 256
-            )})`;
-            sq.value = 0.1;
-          }
-          if (colorChoice === "progressive") {
-            let increment = 0.1;
-            sq.style.backgroundColor = `rgba(0,0,0,${(sq.value += increment)})`;
-          }
-        });
+colorPicker.addEventListener("click", function () {
+  radioColorPicker.checked = true;
+});
+
+function createGrid(size) {
+  containerGrid.innerHTML = "";
+  for (let x = 0; x < size; x++) {
+    for (let y = 0; y < size; y++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.classList.add("grid-on");
+      square.style.height = `${containerGrid.offsetHeight / size}px`;
+      square.style.width = `${containerGrid.offsetWidth / size}px`;
+      // Value used for progressive darkening later
+      square.value = 0.1;
+
+      containerGrid.appendChild(square);
+
+      // Get a node list with all squares on the grid
+      squaresGrid = document.querySelectorAll(".square");
+    }
+  }
+  colourSquare();
+}
+
+function colourSquare() {
+  squaresGrid.forEach((sq) => {
+    ["mousedown", "mouseover"].forEach(function (eventName) {
+      sq.addEventListener(eventName, function () {
+        if (!canColour && eventName !== "mousedown") return;
+
+        const colorChoice = document.querySelector(
+          ".color-choice:checked"
+        ).value;
+
+        if (colorChoice === "color") {
+          sq.style.backgroundColor = `${colorPicker.value}`;
+          sq.value = 0.1;
+        }
+        if (colorChoice === "eraser") {
+          sq.style.backgroundColor = `var(--baby-powder)`;
+          sq.value = 0.1;
+        }
+        if (colorChoice === "random") {
+          sq.style.backgroundColor = `rgb(${Math.floor(
+            Math.random() * 256
+          )},${Math.floor(Math.random() * 256)},${Math.floor(
+            Math.random() * 256
+          )})`;
+          sq.value = 0.1;
+        }
+        if (colorChoice === "progressive") {
+          let increment = 0.1;
+          sq.style.backgroundColor = `rgba(0,0,0,${(sq.value += increment)})`;
+        }
       });
     });
-  }
+  });
+}
 
-  function init() {
-    createGrid(initialGrid);
-    rangeGrid.value = initialGrid;
-  }
+function init() {
+  createGrid(initialGrid);
+  rangeGrid.value = initialGrid;
+}
 
-  init();
-}, 500);
+init();
